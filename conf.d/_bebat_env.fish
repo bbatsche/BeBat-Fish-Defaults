5fdff5d2e6aa6b69199b209fc496354810873bc6
Ben Batschelet <bartleby84@gmail.com>
2021-06-11 23:00:57 -0500

    Use TextMate as default visual editor

--- conf.d/_bebat_env.fish
+++ conf.d/_bebat_env.fish
@@ -2,3 +2,4 @@ set -gx COMPOSER_MEMORY_LIMIT -1
 set -gx NVM_DIR /usr/local/opt/nvm
 set -gx DYLD_FALLBACK_LIBRARY_PATH /usr/local/opt/openssl/lib
 set -gx PATH $PATH "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources"
+set -gx VISUAL "mate -w"
