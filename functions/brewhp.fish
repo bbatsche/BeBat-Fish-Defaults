dc01fa1a1c8e8c430c6973019771d6aadc11814a
Ben Batschelet <batschel@adobe.com>
2021-09-09 01:49:39 -0500

    Apparently version is a reserved variable in Fish

--- functions/brewhp.fish
+++ functions/brewhp.fish
@@ -1,8 +1,8 @@
-function brewhp -a version -d "Swap the currently active version of PHP"
+function brewhp -a php_version -d "Swap the currently active version of PHP"
     set brew_prefix (brew --prefix)
     set -U fish_user_paths (string match -vr "^$brew_prefix/opt/php@\d\.\d" $fish_user_paths)
 
-    if set -q version
-        set -U fish_user_paths $brew_prefix/opt/php@$version/bin $brew_prefix/opt/php@$version/sbin $fish_user_paths
+    if set -q php_version
+        set -U fish_user_paths $brew_prefix/opt/php@$php_version/bin $brew_prefix/opt/php@$php_version/sbin $fish_user_paths
     end
 end
