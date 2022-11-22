f8f920db3b326a0132f68e18dde39e501e89b037
Ben Batschelet <batschel@adobe.com>
2022-11-22 10:17:06 -0600

    Add homebrew shellenv

--- conf.d/_bebat_env.fish
+++ conf.d/_bebat_env.fish
@@ -3,3 +3,5 @@ set -gx NVM_DIR /usr/local/opt/nvm
 set -gx DYLD_FALLBACK_LIBRARY_PATH /usr/local/opt/openssl/lib
 set -gx PATH $PATH "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources"
 set -gx VISUAL "code -w"
+
+eval (/usr/local/bin/brew shellenv)
