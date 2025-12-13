eafe4cb47c4e152c4689ed68a2204c24fe7fc600
Ben Batschelet <bartleby84@gmail.com>
2025-12-12 23:35:17 -0600

    Use brew prefix

--- conf.d/_bebat_env.fish
+++ conf.d/_bebat_env.fish
@@ -1,7 +1,7 @@
 set -gx COMPOSER_MEMORY_LIMIT -1
-set -gx NVM_DIR /usr/local/opt/nvm
+set -gx NVM_DIR "{$HOMEBREW_PREFIX}/opt/nvm"
 set -gx DYLD_FALLBACK_LIBRARY_PATH /usr/local/opt/openssl/lib
 set -gx PATH $PATH "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources"
 set -gx VISUAL "code -w"
 
-eval (/usr/local/bin/brew shellenv)
+eval ("{$HOMEBREW_PREFIX}/bin/brew" shellenv)
