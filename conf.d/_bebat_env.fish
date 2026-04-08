beeb9d0277117e522c5f31ed7bdd49449553748b
Ben Batschelet <bartleby84@gmail.com>
2026-04-08 08:54:51 -0500

    Can't use HOMEBREW_PREFIX if it hasn't been initialized first

--- conf.d/_bebat_env.fish
+++ conf.d/_bebat_env.fish
@@ -1,7 +1,7 @@
+eval (brew shellenv)
+
 set -gx COMPOSER_MEMORY_LIMIT -1
-set -gx NVM_DIR "{$HOMEBREW_PREFIX}/opt/nvm"
+set -gx NVM_DIR "$HOMEBREW_PREFIX/opt/nvm"
 set -gx DYLD_FALLBACK_LIBRARY_PATH /usr/local/opt/openssl/lib
 set -gx PATH $PATH "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources"
 set -gx VISUAL "code -w"
-
-eval (brew shellenv)
